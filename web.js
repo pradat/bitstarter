cb9463534c30c15d2b639d28b6c3b4fd346136bd
pradat <ad.pradat@gmail.com>
2013-07-06 17:41:15 +0000

    Web.js calls html

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
+var fs = require('fs');
+
+var outfile = "index.html";
 
 var app = express.createServer(express.logger());
 
+console.log(fs.readFileSync(outfile).toString());
+
+console.log("Test");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(fs.ReadFileSync(outfile).toString());
    });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 3000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
